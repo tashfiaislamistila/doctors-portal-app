fd432d6ba67d3dd8a9043fb83cd1c8d9ba035ea2
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-10 16:31:13 -0700

    appointment page and route set

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import About from "./Pages/About/About";
+import Appointment from "./Pages/Appointment/Appointment";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className=''>
+
+    <div className='max-w-7xl mx-auto px-12 '>
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="appointment" element={<Appointment />} />
         <Route path="login" element={<Login />} />
       </Routes>
     </div>
