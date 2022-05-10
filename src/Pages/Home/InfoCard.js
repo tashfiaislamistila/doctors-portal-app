4e181ca33d67f67839eb33e2666aae8da4dfe674
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-10 01:07:04 -0700

    card title fixed

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ img, cardTitle }) => {
     return (
         <div class="card lg:card-side bg-base-100 shadow-xl bg-accent">
-            <figure><img src={img} alt="Album" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">New album is released!</h2>
+            <figure className='pl-5'>
+                <img src={img} alt="Album" />
+            </figure>
+            <div class="card-body text-white">
+                <h2 class="card-title">{cardTitle}</h2>
                 <p>Click the button to listen on Spotiwhy app.</p>
-
             </div>
         </div>
     );
