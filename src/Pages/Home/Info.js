c1b28fd6177e593f068d612555282e11e85cedb8
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-10 01:13:17 -0700

    card modification

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,10 +6,10 @@ import phone from '../../assets/icons/phone.svg'
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard cardTitle="Opening Hours" img={clock}></InfoCard>
-            <InfoCard cardTitle="Our Locations" img={marker}></InfoCard>
-            <InfoCard cardTitle="Contact Us" img={phone}></InfoCard>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCard cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
+            <InfoCard cardTitle="Our Locations" bgClass="bg-accent" img={marker}></InfoCard>
+            <InfoCard cardTitle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
         </div>
     );
 };
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const InfoCard = ({ img, cardTitle }) => {
+const InfoCard = ({ img, cardTitle, bgClass }) => {
     return (
-        <div class="card lg:card-side bg-base-100 shadow-xl bg-accent">
+        <div class={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
             <figure className='pl-5'>
                 <img src={img} alt="Album" />
             </figure>
