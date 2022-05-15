15ebed42cffb25ca91c6553aaed2ff8f3ce15062
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-15 00:45:19 -0700

    change API

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -6,9 +6,10 @@ import Service from './Service';
 const AvailableAppointments = ({ date }) => {
     const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
+    const formatedDate = format(date, 'PP');
 
     useEffect(() => {
-        fetch('http://localhost:5000/service')
+        fetch(`http://localhost:5000/available?date=${formatedDate}`)
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
