4a916b070b15598280d257ae6079a62619976f8d
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-15 20:51:16 -0700

    made admin

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -4,7 +4,13 @@ import Loading from '../Shared/Loading';
 import UserRow from './UserRow';
 
 const Users = () => {
-    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    })
+        .then(res => res.json()));
     if (isLoading) {
         return <Loading></Loading>
     }
@@ -28,6 +34,7 @@ const Users = () => {
                             users.map(user => <UserRow
                                 key={user._id}
                                 user={user}
+                                refetch={refetch}
                             ></UserRow>)
                         }
                     </tbody>
