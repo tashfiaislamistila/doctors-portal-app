069116d91cacf3fe371574667735092934fa1fb5
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-18 23:24:22 -0700

    payment intrigation update

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,7 +1,13 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+
+const stripePromise = loadStripe('pk_test_51L0xX5CQ1vBM2PUYBjELFcifPOEbl8yCzAGlsKuRkA8ks7NkCJ4gzhrXvEWRgtnCV7iZkcqOWQE1ZN3iChgozUxa00Y7h3HvFA');
 
 const Payment = () => {
     const { id } = useParams();
@@ -30,6 +36,9 @@ const Payment = () => {
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
                 </div>
             </div>
         </div>
---
+++ src/Pages/Dashboard/___payment_steps.js
@@ -0,0 +1,12 @@
+/**
+ * 1> install stripe react stripe js
+ * 2> open stripe account on stripe website
+ * 3> get publishable key pk__
+ * 4> create elements wrapper using publishable key
+ * 5> Create Checkout form using card element, useStripe, useElements
+ * 6> get card elements info(credit card info)
+ * --------------------------------------------------------------------------
+ * 7> get credit card info/error 
+ * 
+ * 
+ * ***/
